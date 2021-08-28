445c72a32dc00c9c921c8a017ebef09945682c35
Eder Sena <eder.sena@live.com>
2021-08-27 22:45:01 -0300

    create bn

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const cool = require('cool-ascii-faces');
+
+var firebaseKey = path.join(__dirname, 'assets/serviceAccountKey.json')
+var admin = require("firebase-admin");
+
+var serviceAccount = require(firebaseKey);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://icarrosujo-default-rtdb.firebaseio.com/"
+});
+
+admin
+  .database()
+  .ref('usuarios')
+  .on('child_added', snapShot => {
+    snapShot.ref.update({ atualizado: true });
+});
+
+express()
+  .use(express.static(path.join(__dirname, 'assets')))
+  .get('/users', async (req, res) => {
+  const users = await admin.database().ref('usuarios').once('value');
+  res.send(users.val());
+})
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
