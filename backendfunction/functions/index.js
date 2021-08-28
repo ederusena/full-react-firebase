d6ee0027fbcc08726d37d053e0618b691b943f7f
Eder Sena <eder.sena@live.com>
2021-08-28 14:35:40 -0300

    Signup

--- backendfunction/functions/index.js
+++ backendfunction/functions/index.js
@@ -1,7 +1,22 @@
 const functions = require("firebase-functions");
+const { initializeApp } = require("firebase/app");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9rR2A65DoGS9II3rtNKiK9kRXSDbwRhM",
+  authDomain: "icarrosujo.firebaseapp.com",
+  databaseURL: "https://icarrosujo-default-rtdb.firebaseio.com",
+  projectId: "icarrosujo",
+  storageBucket: "icarrosujo.appspot.com",
+  messagingSenderId: "1053429207595",
+  appId: "1:1053429207595:web:af5f2fdd3cd15eba55506e",
+  measurementId: "G-B49H3Z627G"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const express = require("express");
 const app = express();
@@ -46,6 +61,28 @@ app.post("/scream", (req, res) => {
       console.error(err);
     });
 });
+
+// Signup route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  // TODO: validate data
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then((data) => {
+      return res.status(201).json({ message: `user ${data.user.uid} signed up successfully` });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+});
+
+    
 
 // https://baseurl.com/api/
 exports.api = functions.region('us-central1').https.onRequest(app);
