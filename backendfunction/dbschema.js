a23ddd236c42df043227cb7e896c2e939bd3a9f9
Eder Sena <eder.sena@live.com>
2021-08-28 14:24:30 -0300

    get and create

---
+++ backendfunction/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2020-03-15T10:59:52.798Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
